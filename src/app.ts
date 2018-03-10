abaffd858225883dbb5393938094d20d1afd56fd
kimtaesu <kimtaesoo188@gmail.com>
2018-03-10 21:28:42 +0900

    dot env 적용

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import * as express from "express";
 import * as homeController from "./controllers/home";
+import * as dotenv from "dotenv";
+
+dotenv.config({path: ".env.example"});
 
 const app = express();
 
