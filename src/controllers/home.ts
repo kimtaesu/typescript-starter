49266f55680c51b28ea95643238b0d6a625878c8
kimtaesu <kimtaesoo188@gmail.com>
2018-03-10 15:18:52 +0900

    Watch restart

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -5,5 +5,5 @@ import {Request, Response} from "express";
  * Home page.
  */
 export let index = (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.send("tasdayler World!");
 };
