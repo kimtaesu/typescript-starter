161460f86b62eb2407839a59966d8d5e445912d1
kimtaesu <kimtaesoo188@gmail.com>
2018-03-10 23:51:45 +0900

    test 환경

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
-import app from "./app";
-import * as express from "express";
-import * as errorHandler from "errorhandler";
+const app = require("./app");
 
 const port: number = Number(process.env.PORT) || 3000;
 
-app.use(errorHandler());
-
 exports = app.listen(port, () => {
-    console.log(`mongo db url : ${process.env.MONGODB_URI}`)
+    console.log(`mongo db url ${process.env.MONGODB_URI}`)
     console.log(("  App is running at http://localhost:%d in"), port);
     console.log("  Press CTRL-C to stop\n");
 });
