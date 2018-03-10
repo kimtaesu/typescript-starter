2610e79c9711d768fedd48b3572857129dc2c56c
kimtaesu <kimtaesoo188@gmail.com>
2018-03-10 22:29:16 +0900

    bodyparser json 추가

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,19 @@ import * as express from "express";
 import * as homeController from "./controllers/home";
 import * as dotenv from "dotenv";
 import * as logger from "morgan";
+import * as bodyParser from "body-parser";
 
 dotenv.config({path: ".env.example"});
 
 const app = express();
 
 // morgan logger
-app.use(logger("dev"));
+app.use(logger("dev"))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", homeController.index);
+app.post("/test", homeController.test);
 
 export default app;
--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -9,3 +9,7 @@ import {WriteError} from "mongodb";
 export let index = (req: Request, res: Response, next: NextFunction) => {
     res.send("Hellow");
 };
+export let test = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body)
+    res.send("test!");
+};
